0abc8ab5cf31a229852e5401a36beae65d3258e9
rubina02akter <rubinaakter0108@gmail.com>
2025-05-16 23:30:47 +0600

    fao

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
 
-
 export default function nofound() {
   return (
     <div>
-      <h2>this page isn't invalid</h2>
-      <Link href={"/"}> Back To Home</Link>
+      <h2>{`this page isn't invalid`}</h2>
+      <Link href={"/"}>Back To Home</Link>
     </div>
   );
 }
